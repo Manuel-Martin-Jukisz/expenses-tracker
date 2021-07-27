c0cc94637af2da88c0610132ecf5e036f1e9e653
Manuel Martin Jukisz <manujukisz@outlook.com>
2021-07-27 23:19:47 +0100

    We are able to pass the data of the for till the App.js but we are not changing the expeses object jet

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,19 +1,59 @@
 import './Css/ExpenseForm.css'
+import { useState } from 'react';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmaount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // })
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value)
+        // setUserInput((prevState) => {
+        //     return {...prevState, enteredTitle: event.target.value};
+        // })
+    }
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmaount(event.target.value)
+    }
+    
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData= {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+
+        props.onSaveExpenseDate(expenseData);
+        setEnteredTitle('');
+        setEnteredAmaount('');
+        setEnteredDate('');
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' />
+                    <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' />
+                    <input type='number' min='0.01' step='0.01'value={enteredAmount} onChange={amountChangeHandler}/>
                     <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min="2019-01-01"  max="2022-12-31"/>
+                    <input type='date' min="2019-01-01"  max="2022-12-31" value={enteredDate} onChange={dateChangeHandler}/>
                 </div>
                 </div>
             </div>
