df6a7d43b2843b349ff7d40f2a5b8d677610b5a6
Manuel Martin Jukisz <manujukisz@outlook.com>
2021-07-29 21:06:59 +0100

    Created a different component for the rendering of the Expenses List, which now use a different code to render conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,9 @@
 import { useState } from 'react';
 
 import ExpensesFilter from './ExpensesFilter';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import './css/Expenses.css';
-
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -13,34 +12,23 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
-    return expense.date.getFullYear().toString() === filteredYear  
-  })
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
-  let expensesContent = <p>No expenses found for this year.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-      ));
-  }
-  
   return (
-    <div>
-      <Card className="expenses">
-        <ExpensesFilter
-          selectedYear={filteredYear}
-          onFilterYear={filteredYearHandler}
-        />
+    <li>
+      <div>
+        <Card className="expenses">
+          <ExpensesFilter
+            selectedYear={filteredYear}
+            onFilterYear={filteredYearHandler}
+          />
 
-        {expensesContent}
-      </Card>
-    </div>
+          <ExpensesList items={filteredExpenses} />
+        </Card>
+      </div>
+    </li>
   );
 };
 export default Expenses;
