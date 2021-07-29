aa8b308d43e679679db04d1b1cf80c4af9600adf
Manuel Martin Jukisz <manujukisz@outlook.com>
2021-07-29 21:40:25 +0100

    The form is now Conditional content as we use a button to make it appear and to make it disappear in the cancel button or went is submitted

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,27 @@
+import { useState } from 'react';
 import './Css/NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+  const [formVisible, setFormVisible] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setFormVisible(false)
+  };
+
+  const visabilityHandler = () => {
+    formVisible === false ? setFormVisible(true) : setFormVisible(false)
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseDate={saveExpenseDataHandler} />
+      {!formVisible && <button onClick={visabilityHandler}>Add New Expense</button>}
+      {formVisible && <ExpenseForm onSaveExpenseDate={saveExpenseDataHandler} onCancel={visabilityHandler} />}
     </div>
   );
 };
